965e2148b10734977cafadcf7575bc74bc4b6bb7
etmula <mountainroad@hotmail.co.jp>
2019-09-23 23:41:47 +0900

    add function virtualwrite and BLYNK_WRITEs

--- GEGU.ino
+++ GEGU.ino
@@ -20,11 +20,16 @@
 #define  STROBE_TM D5
 #define  CLOCK_TM D6
 #define  DIO_TM D7
+//values
+//viretualPin data[PinNo]
+String data[40];
 //RTC
 WidgetRTC rtc;
 
 //Constructor object
 TM1638plus tm(STROBE_TM, CLOCK_TM , DIO_TM);
+//overwrite all data to viretualPin
+void virtualwrite();
 void setup()
 {
 
@@ -41,4 +46,94 @@ void loop()
 BLYNK_CONNECTED() {
   // Synchronize time on connection
   rtc.begin();
+}
+void virtualwrite(){
+  Blynk.virtualWrite(V0,data[0]);
+  Blynk.virtualWrite(V1,data[1]);
+  Blynk.virtualWrite(V2,data[2]);
+  Blynk.virtualWrite(V3,data[3]);
+  Blynk.virtualWrite(V4,data[4]);
+  Blynk.virtualWrite(V5,data[5]);
+  Blynk.virtualWrite(V6,data[6]);
+  Blynk.virtualWrite(V7,data[7]);
+  Blynk.virtualWrite(V10,data[10]);
+  Blynk.virtualWrite(V11,data[11]);
+  Blynk.virtualWrite(V12,data[12]);
+  Blynk.virtualWrite(V13,data[13]);
+  Blynk.virtualWrite(V20,data[20]);
+  Blynk.virtualWrite(V21,data[21]);
+  Blynk.virtualWrite(V22,data[22]);
+  Blynk.virtualWrite(V30,data[30]);
+  Blynk.virtualWrite(V31,data[31]);
+  Blynk.virtualWrite(V32,data[32]);
+  Blynk.virtualWrite(V35,data[30]);
+  Blynk.virtualWrite(V36,data[31]);
+  Blynk.virtualWrite(V37,data[32]);
+}
+
+BLYNK_WRITE(V0){
+  data[0] = param.asStr();
+}
+BLYNK_WRITE(V1){
+  data[1] = param.asStr();
+}
+BLYNK_WRITE(V2){
+  data[2] = param.asStr();
+}
+BLYNK_WRITE(V3){
+  data[3] = param.asStr();
+}
+BLYNK_WRITE(V4){
+  data[4] = param.asStr();
+}
+BLYNK_WRITE(V5){
+  data[5] = param.asStr();
+}
+BLYNK_WRITE(V6){
+  data[6] = param.asStr();
+}
+BLYNK_WRITE(V7){
+  data[7] = param.asStr();
+}
+BLYNK_WRITE(V10){
+  data[10] = param.asStr();
+}
+BLYNK_WRITE(V11){
+  data[11] = param.asStr();
+}
+BLYNK_WRITE(V12){
+  data[12] = param.asStr();
+}
+BLYNK_WRITE(V13){
+  data[13] = param.asStr();
+}
+BLYNK_WRITE(V20){
+  data[20] = param.asStr();
+}
+BLYNK_WRITE(V21){
+  data[21] = param.asStr();
+}
+BLYNK_WRITE(V22){
+  data[22] = param.asStr();
+}
+BLYNK_WRITE(V30){
+  data[30] = now();
+  virtualwrite();
+}
+BLYNK_WRITE(V31){
+  data[31] = now();
+  virtualwrite();
+}
+BLYNK_WRITE(V32){
+  data[32] = now();
+  virtualwrite();
+}
+BLYNK_WRITE(V35){
+  data[30] = param.asStr();
+}
+BLYNK_WRITE(V36){
+  data[31] = param.asStr();
+}
+BLYNK_WRITE(V37){
+  data[32] = param.asStr();
 }
